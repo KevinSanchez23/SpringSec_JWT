5fc35317b0c73157d27c9dd4c61e99e0c6c84d1a
KevinSanchez23 <99674560+KevinSanchez23@users.noreply.github.com>
2026-03-26 20:30:35 -0600

    Fisrt commit

---
+++ src/test/java/com/Kuro/SpringSec_JWT/SpringSecJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kuro.SpringSec_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
